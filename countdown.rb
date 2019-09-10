93a9fe95127d83e7d881e1b472c7256a74a76166
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-10 12:52:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown(integer)
-  counter = integer
   message = "HAPPY NEW YEAR!"
-  while counter > 0
+  while integer > 0
     puts "#{integer} SECOND(S)"
-    counter -= 1
+    integer -= 1
   end
   message
 end
